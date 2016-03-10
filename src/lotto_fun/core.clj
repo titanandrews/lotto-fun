13dfb213eb5a977895f7f88c0d7258f6b3471a97
titanandrews <titanandrews@gmail.com>
2016-03-09 21:15:53 -0500

    Download latest csv file

--- src/lotto_fun/core.clj
+++ src/lotto_fun/core.clj
@@ -2,10 +2,11 @@
   (:gen-class))
   (use 'clojure.java.io)
   (require '[clojure.string :as str])
+  (require '[clojure.java.io :as io])
 
 (defn process-csv
-  [handle-line]
-    (with-open [rdr (reader "NCELPowerball.csv")]
+  [handle-line filename]
+    (with-open [rdr (reader filename)]
       (doseq [line (line-seq rdr)]
         (handle-line
           (drop 1
@@ -21,11 +22,20 @@
       (= (count item) 6 ))
       data)))
 
+(defn copy-uri-to-file
+  [uri file]
+  (with-open [in (io/input-stream uri)
+              out (io/output-stream file)]
+    (io/copy in out)))
+
 (defn get-winning-seqs
   []
+  (println "Downloading latest winning sequences...")
+  (copy-uri-to-file "http://www.nc-educationlottery.org/powerball_download.aspx"
+    "NCELPowerball.csv")
   (def lines [] )
   (process-csv (fn [line]
-    (def lines (concat lines [line]))))
+    (def lines (concat lines [line]))) "NCELPowerball.csv")
   (clean-data lines))
 
 (defn -main

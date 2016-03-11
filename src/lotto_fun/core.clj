51c6621f4aa40a90fcf4e041a39a917cad27c646
titanandrews <titanandrews@gmail.com>
2016-03-10 20:56:27 -0500

    List most winning numbers.

--- src/lotto_fun/core.clj
+++ src/lotto_fun/core.clj
@@ -4,7 +4,7 @@
   (require '[clojure.string :as str])
   (require '[clojure.java.io :as io])
 
-(defn process-csv
+(defn- process-csv
   [handle-line filename]
     (with-open [rdr (reader filename)]
       (doseq [line (line-seq rdr)]
@@ -13,7 +13,7 @@
             (drop-last 2
               (str/split line #",")))))))
 
-(defn clean-data
+(defn- clean-data
   [data]
   ; Drop the header,
   ; and drop lines from end. Do it this way in case format changes.
@@ -22,13 +22,15 @@
       (= (count item) 6 ))
       data)))
 
-(defn copy-uri-to-file
+(defn- copy-uri-to-file
   [uri file]
   (with-open [in (io/input-stream uri)
               out (io/output-stream file)]
     (io/copy in out)))
 
 (defn get-winning-seqs
+  "Returns sequences of past winning numbers from the Powerball CSV file
+  downloaded from the internet."
   []
   (println "Downloading latest winning sequences...")
   (copy-uri-to-file "http://www.nc-educationlottery.org/powerball_download.aspx"
@@ -38,6 +40,47 @@
     (def lines (concat lines [line]))) "NCELPowerball.csv")
   (clean-data lines))
 
+(defn- keep-track
+  [num num-map]
+  (update num-map num (fn [num]
+    (if (nil? num) 1 (inc num)))))
+
+(defn count-winning-numbers-excluding-powerball
+  "Returns a list of pairs of the numbers paired with how many times
+  it was picked, sorted by most picks first."
+  [seqs]
+  ; TODO How do I make this a local var???
+  (def num-map nil)
+  (doseq [ seq seqs ]
+    (doseq [ num (drop-last seq) ]
+      (def num-map (keep-track num num-map))))
+  (sort-by val > num-map))
+
+(defn count-powerballs-only
+  "Returns a list of pairs of only the powerball numbers paired with how many times
+  it was picked, sorted by most picks first."
+  [seqs]
+  ; TODO How do I make this a local var???
+  (def num-map nil)
+  (doseq [ seq seqs ]
+    (doseq [ num (drop 5 seq) ]
+      (def num-map (keep-track num num-map))))
+  (sort-by val > num-map))
+
 (defn -main
   [& args]
-  (println (get-winning-seqs)))
+  (let [all-nums (get-winning-seqs)]
+    (let [winning-nums (count-winning-numbers-excluding-powerball all-nums)]
+      (println "Top 20 numbers")
+      (println "Number\tTimes picked")
+      (doseq [ num-pair (take 20 winning-nums) ]
+        (println num-pair)))
+
+    (let [winning-nums (count-powerballs-only all-nums)]
+      (println "Top 20 Powerball numbers")
+      (println "Number\tTimes picked")
+      (doseq [ num-pair (take 20 winning-nums) ]
+        (println num-pair)))))
+
+    ; TODO More interesting stats coming soon!
+
--- test/lotto_fun/core_test.clj
+++ test/lotto_fun/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [lotto-fun.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-count-winning-numbers-excluding-powerball
+  (testing "should count numbers"
+    (def counts (count-winning-numbers-excluding-powerball [["1" "2" "3" "4" "5" "6"]
+                                                            ["1" "2" "7" "8" "9" "10"]
+                                                            ["9" "1" "12" "13" "14" "15"]]))
+    (is (= counts [["1" 3] ["9" 2] ["2" 2] ["3" 1] ["4" 1] ["8" 1]
+                   ["14" 1] ["7" 1] ["5" 1] ["12" 1] ["13" 1]]))))
+
+
+(deftest test-count-powerballs-only
+  (testing "should count only powerball numbers"
+    (def counts (count-powerballs-only [["1" "2" "3" "4" "5" "6"]
+                                        ["1" "2" "7" "8" "9" "10"]
+                                        ["9" "1" "12" "13" "14" "15"]
+                                        ["9" "1" "12" "13" "14" "15"]]))
+    (is (= counts [["15" 2] ["6" 1] ["10" 1]]))))

4c481682056ae7f9667ad6f6c6a32568739a030f
titanandrews <titanandrews@gmail.com>
2016-03-24 17:50:42 -0400

    use let

--- test/lotto_fun/core_test.clj
+++ test/lotto_fun/core_test.clj
@@ -4,17 +4,19 @@
 
 (deftest test-count-winning-numbers-excluding-powerball
   (testing "should count numbers"
-    (def counts (count-winning-numbers-excluding-powerball [["1" "2" "3" "4" "5" "6"]
-                                                            ["1" "2" "7" "8" "9" "10"]
-                                                            ["9" "1" "12" "13" "14" "15"]]))
-    (is (= counts [["1" 3] ["9" 2] ["2" 2] ["3" 1] ["4" 1] ["8" 1]
-                   ["14" 1] ["7" 1] ["5" 1] ["12" 1] ["13" 1]]))))
+    (let [counts
+      (count-winning-numbers-excluding-powerball [["1" "2" "3" "4" "5" "6"]
+                                                  ["1" "2" "7" "8" "9" "10"]
+                                                  ["9" "1" "12" "13" "14" "15"]])]
+      (is (= counts [["1" 3] ["9" 2] ["2" 2] ["3" 1] ["4" 1] ["8" 1]
+                     ["14" 1] ["7" 1] ["5" 1] ["12" 1] ["13" 1]])))))
 
 
 (deftest test-count-powerballs-only
   (testing "should count only powerball numbers"
-    (def counts (count-powerballs-only [["1" "2" "3" "4" "5" "6"]
-                                        ["1" "2" "7" "8" "9" "10"]
-                                        ["9" "1" "12" "13" "14" "15"]
-                                        ["9" "1" "12" "13" "14" "15"]]))
-    (is (= counts [["15" 2] ["6" 1] ["10" 1]]))))
+    (let [counts
+      (count-powerballs-only [["1" "2" "3" "4" "5" "6"]
+                              ["1" "2" "7" "8" "9" "10"]
+                              ["9" "1" "12" "13" "14" "15"]
+                              ["9" "1" "12" "13" "14" "15"]])]
+      (is (= counts [["15" 2] ["6" 1] ["10" 1]])))))
